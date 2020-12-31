2c09a28239adf1d98643f7f8c09347783318e430
Jordankwek <kwekjordan@gmail.com>
2021-01-01 02:44:09 +0800

    #Commit 6 - Added a pie chart into my website as well as a form that prompts for user input and display the data as the pie chart.

--- README.md
+++ README.md
@@ -5,6 +5,12 @@
 
 ## Credits
 
+
 The images used in this website were obtained from: 
 
 1) https://www.freshbooks.com/blog/wp-content/uploads/2018/02/tracking-expenses-blog-852x568.png
+
+Resources used: 
+1) https://www.youtube.com/watch?v=gXkqy0b4M5g
+2) https://codeactually.com/googlecharts.html
+3) https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp
---
+++ chart.js
@@ -0,0 +1,28 @@
+google.charts.load('current', { 'packages': ['corechart'] });
+//google.charts.setOnLoadCallback(drawChart);
+
+// Draw the chart and set the chart values
+function drawChart() {
+
+  food = parseFloat(document.getElementById('foodvar').value);
+  beauty = parseFloat(document.getElementById('beautyvar').value);
+  transport = parseFloat(document.getElementById('transportvar').value);
+  entertainment = parseFloat(document.getElementById('entertainmentvar').value);
+  other = parseFloat(document.getElementById('othervar').value);
+
+  var data = google.visualization.arrayToDataTable([
+    ['Expenses', 'Amount spent per day'],
+    ['Food', food],
+    ['Beauty', beauty],
+    ['Transportation', transport],
+    ['Entertainment', entertainment],
+    ['Others', other]
+  ]);
+
+  // Optional; add a title and set the width and height of the chart
+
+
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data);
+}
